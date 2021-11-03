ceff9e33560d90090eb3a4b9c0fc9299eea3c96d
Pepco Planning <mborycki@pepco.eu>
2021-11-03 13:44:00 +0100

    Finalized the GUI and added logo

--- functions.py
+++ functions.py
@@ -1,5 +1,9 @@
 import os
-
+from tkinter import *
+import pandas as pd
+import numpy as np
+from tkinter import messagebox as msb
+import time
 # checking if the path is correct
 
 
@@ -27,3 +31,113 @@ def setStartEndWeeks():
     #
     # return startEndWeeks
 
+# displaying a text in a TextBox
+def display_text(master, content):
+    infoField = Text(master, bg='light grey', width=50, height=6, font=("Helvetica", 10)) # , padx=8, pady=8
+    infoField.place(relx=0.4, rely=0.75)
+    infoField.insert(1.0, content)
+    # T.tag_configure("center", justify="center")
+    # infoField.tag_add("center", 1.0, "end")
+    return infoField
+
+def check_path(master, folderPath, fileName):
+    if len(fileName) < 1:
+        fileName = 'no_file'
+
+    dirPath = folderPath + fileName
+    if os.path.exists(dirPath):
+        requiaredFiles = ["StoreCountryGroup.csv", "GradeCutOff.csv", "GradeSellOff.csv", "DS_PARAM.csv"]
+        text1 = ""
+
+        for file in requiaredFiles:
+            if not(os.path.isfile(folderPath + file)):
+                text1 = text1 + file + ', \n'
+        if text1 == "":
+            display_text(master, f"Good! \n\nThe folder path exist and there is no missing files\n\nYou can click on \n'Submit' button")
+        else:
+            display_text(master, f"The folder path exist but there are \nsome missing files such as: \n\n{text1}")
+
+    else:
+        if fileName == 'no_file':
+            display_text(master, "Error! \n\nThe chosen file name does not exist \n\nPlease try type the file name again")
+        else:
+            display_text(master, "Error! \n\nThe chosen directory does not exist \n\nPlease try type the folder path again")
+
+    return dirPath
+
+def setTextInput(text, e4):
+    e4.delete(0,"end")
+    e4.insert(0, text)
+
+# Instruction
+model_description = "'Folder path': \nWhere do you keep input files.\n" \
+                    "Example: 'c:\Mariusz\MyProjects\LostSales\input files'\n\n" \
+                    "'Database file name': \nA name of the database file\n" \
+                    "Example: 'StockAndSales_PQ1.zip'\n\n" \
+                    "'Download db': \nDo you have a .csv database file or you are going to download a new one?\n\n" \
+                    "'check' button: \nThe button is checking whether the chosen path is correct and if it contains all of the necessary files"
+
+def calculation(folder):
+    msb.showinfo("Information", "Wait a bit. The Model is running")
+
+    stockAndSales = pd.read_csv(folder, compression='zip')
+    # -Calculations--------------------------------------------
+    stockAndSales.rename(
+        columns={'StrNumber': 'Store', 'SalesValue': 'SalesR', 'SalesQty': 'SalesQ', 'StockQty': 'StockQ'},
+        inplace=True)
+
+    # make sure you can do this:
+    stockAndSales = stockAndSales[(stockAndSales['SalesR'] > 0) & (stockAndSales['SalesQ'] > 0)]
+    stockAndSales['StockQ'] = stockAndSales['StockQ'].apply(lambda x: 0 if x < 0 else x)
+
+    # -Total Sales per Category/Store--------------------------
+    temp = stockAndSales.groupby(['CategoryID', 'Store']).agg({'SalesR': 'sum', 'SalesQ': 'sum'}).reset_index()
+    temp.rename({'SalesR': 'Total_CatSalesR', 'SalesQ': 'Total_CatSalesQ'}, axis=1, inplace=True)
+    stockAndSales = pd.merge(stockAndSales, temp, on=['CategoryID', 'Store'], how='left').sort_values(
+        by=['Store', 'CategoryID'])
+    del temp
+
+    # -Total Sales per Store-----------------------------------
+    temp = stockAndSales.groupby(['Store']).agg({'SalesR': 'sum'}).reset_index()
+    temp.rename({'SalesR': 'Total_StoreSalesR'}, axis=1, inplace=True)
+    stockAndSales = pd.merge(stockAndSales, temp, on=['Store'], how='left').sort_values(by=['Store', 'CategoryID'])
+    del temp
+
+    # -Aggregation---------------------------------------------
+    stockAndSales['SalesAUP'] = stockAndSales.SalesR / stockAndSales.SalesQ
+    stockAndSales['Total_SalesAUP'] = stockAndSales.Total_CatSalesR / stockAndSales.Total_CatSalesQ
+    stockAndSales['SellOff'] = stockAndSales.SalesQ / (stockAndSales.StockQ + stockAndSales.SalesQ)
+
+    stockAndSales['CatSalesRatio'] = stockAndSales.Total_CatSalesR / stockAndSales.Total_StoreSalesR  # Profiles
+    stockAndSales['WeekSalesRatio'] = stockAndSales.SalesR / stockAndSales.Total_CatSalesR  # Profiles_Sales Ratio
+
+    # -Temporary added manually--------------------------------
+    stockAndSales['DesiredSellOff'] = 0.15
+    stockAndSales['MLQ'] = 120
+    stockAndSales['IncreaseFactor'] = 0.10
+
+    # -Final Calculations--------------------------------------
+    stockAndSales['s1'] = np.where(stockAndSales.SellOff > stockAndSales.DesiredSellOff, 1,
+                                   np.where(stockAndSales.StockQ <= stockAndSales.MLQ, 1, 0))
+    stockAndSales['s2'] = np.where(stockAndSales.s1 == 1,
+                                   np.where(stockAndSales.WeekSalesRatio - stockAndSales.CatSalesRatio > 0, 1, 0), 0)
+    stockAndSales['s2'] = np.where(stockAndSales.s1 == 1,
+                                   np.where(stockAndSales.WeekSalesRatio - stockAndSales.CatSalesRatio > 0, 1, 0), 0)
+
+    stockAndSales['ConditionCalc'] = np.where(stockAndSales.s2 == 0, stockAndSales.SalesR, 0)
+    stockAndSales['ConditionMix'] = np.where(stockAndSales.s2 == 0, stockAndSales.WeekSalesRatio, 0)
+    stockAndSales['SalesAdjusted'] = stockAndSales.ConditionCalc / stockAndSales.ConditionMix
+    stockAndSales['SalesAdjusted'].fillna(0, inplace=True)
+
+    stockAndSales['s3_Ret'] = np.where(stockAndSales.s2 == 0, 0,
+                                       stockAndSales.WeekSalesRatio * stockAndSales.SalesAdjusted - stockAndSales.SalesR)
+    stockAndSales['s4_Ret'] = np.where(stockAndSales.s1 == 1,
+                                       np.where(stockAndSales.WeekSalesRatio - stockAndSales.CatSalesRatio < 0,
+                                                stockAndSales.SalesR * stockAndSales.IncreaseFactor, 0), 0)
+    stockAndSales['s4_Q'] = (stockAndSales.s3_Ret + stockAndSales.s4_Ret) / np.where(stockAndSales.SalesAUP == 0,
+                                                                                     stockAndSales.Total_SalesAUP,
+                                                                                     stockAndSales.SalesAUP)
+
+    folderPath = 'c:\Mariusz\MyProjects\LostSales\input files\\'
+    stockAndSales.head().to_csv(folderPath + 'final_df.csv', index=False)
+    msb.showinfo("Information", f"The Model is ready.\nFolder:\n{folderPath}\nFile:\n'final_df.csv'")
--- main.py
+++ main.py
@@ -1,84 +1,64 @@
-import pandas as pd
-import numpy as np
-import gui
-# import daxDownloader as dd
-# import daxQueries as dq
-# import functions as f
-# import StockAndSales
-# import os
-# import datetime
+import tkinter
+from tkinter import *
+import functions as f
+from PIL import Image, ImageTk
+import os
+
+root = Tk()
+root.geometry('800x500')
+root.title("Lost Sales Model")
+
+logo = Image.open('pepco-logo2.jpg')
+logo = ImageTk.PhotoImage(logo)
+logo_label = Label(image=logo)
+logo_label.image = logo
+logo_label.place(relx=0.5, rely=0.1, anchor='center')
+
+#TITLE##############
+label_0 = Label(root, text="Lost Sales", width=20, font=("bold", 20))
+label_0.place(relx=0.5, rely=0.3, anchor='center') # label_0.place(x=120,y=53)
+
+#LABEL 1############
+label_1 = Label(root, text="Folder path", width=20, anchor='e', font=("bold", 10))
+label_1.place(x=65,y=180)
+entry_1 = Entry(root, width=60)
+entry_1.place(x=240, y=180)
+
+#LABEL 2############
+label_2 = Label(root, text="Database file name", width=20, anchor='e', font=("bold", 10))
+label_2.place(x=65,y=230)
+entry_2 = Entry(root, width=60)
+entry_2.place(x=240, y=230)
+
+#LABEL 4############
+label_4 = Label(root, text="Database dates", width=20, anchor='e', font=("bold", 10))
+label_4.place(x=65, y=280)
+entry_4 = Entry(root, textvariable='Yes', width=60, fg='grey')
+entry_4.place(x=240, y=280)
+
+#LABEL 3############
+label_3 = Label(root, text="Download db", width=20, anchor='e', font=("bold", 10))
+label_3.place(x=65,y=330)
+var = IntVar()
+Radiobutton(root, text="Yes", padx = 5, variable=var, value=1, command=lambda:f.setTextInput("Please type here a date you choose. (Eg.: 'Y2021W01', 'Y2021W04')",entry_4)).place(x=235, y=330)
+Radiobutton(root, text="No", padx = 20, variable=var, value=2, command=lambda:f.setTextInput("No need a date", entry_4)).place(x=290, y=330)
+
+#Information Field##
+infoField = Text(root, bg='light grey', width=50, height=6, font=("Helvetica", 10))  # , padx=8, pady=8
+infoField.place(relx=0.4, rely=0.75)
+infoField.insert(1.0, "Hello in the Lost Sales Model\n\nPlease fill in the above fields")
+infoField.tag_configure("tag_name", justify="center")
+infoField.tag_add("tag_name", 1.0, "end")
+
+# Create Buttons
+check_button = Button(root, text="Check Path", width=20, bg='light grey', fg='black', command=lambda:f.check_path(root, entry_1.get(), entry_2.get())) # command=query
+check_button.place(relx=0.25, rely=0.78, anchor='center')
+
+submit_btn = Button(root, text='Submit', width=20, bg='brown', fg='white', command=lambda:f.calculation(f.check_path(root, entry_1.get(), entry_2.get())))
+submit_btn.place(relx=0.25, rely=0.92, anchor='center') # x=180,y=380
+
+help_btn = Button(root, text="Instruction", width=20, bg='light grey', fg='black', command=lambda:[f.display_text(root, f.model_description)]) # command=query
+help_btn.place(relx=0.25, rely=0.85, anchor='center')
 
 
-# print('### Lost Sales ###')
-
-################### odkomentować przed startem
-# print("Witaj użytkowniku! W celu wyliczenia lost sales podążaj za instrukcjami.")
-# print("Ustaw ścieżkę folderu, gdzie będą trzymane wszystkie pliki csv:")
-# folderPath = f.setUpFolderPath()
-# folderPath = r"c:\Mariusz\MyProjects\LostSales\input files" ## f.setUpFolderPath()
-# print("Umieść w folderze ", folderPath, " wymgane pliki .csv: StoreAttributes, GradeCutOff, GradeSellOff, DSParam")
-
-# 0. Pobierz dane:
-################### odkomentować przed startem
-# StockAndSales.run(stockAndSales_path)
-# stockAndSales = pd.read_csv(stockAndSales_path, compression='zip')
-# storeCountryGroup = pd.read_csv(storeCountryGroup_path)
-# cutOff = pd.read_csv(gradeCutOff_path)
-
-# folderPath = r"\\10.2.5.140\zasoby\Planowanie\PERSONAL FOLDERS\Mariusz Borycki\Python Projects\LostSales\input files"
-
-# df = pd.read_excel('c:\Mariusz\MyProjects\LostSales\Lost Sales Model.xlsm', sheet_name='Sheet1')
-# folderPath = df.iloc[0,1]
-# file_name = df.iloc[0,2]
-# stockAndSales_path = f"{folderPath}\{file_name}"
-
-folderPath = r"\\10.2.5.140\zasoby\Planowanie\PERSONAL FOLDERS\Mariusz Borycki\Python Projects\LostSales\input files"
-stockAndSales_path = folderPath + '/StockAndSales_PQ1.zip'
-stockAndSales = pd.read_csv(stockAndSales_path, compression='zip')
-# -Calculations--------------------------------------------
-stockAndSales.rename(columns={'StrNumber':'Store', 'SalesValue':'SalesR', 'SalesQty':'SalesQ', 'StockQty':'StockQ'}, inplace=True)
-
-# make sure you can do this:
-stockAndSales = stockAndSales[(stockAndSales['SalesR'] > 0) & (stockAndSales['SalesQ'] > 0)]
-stockAndSales['StockQ'] = stockAndSales['StockQ'].apply(lambda x: 0 if x < 0 else x)
-
-# -Total Sales per Category/Store--------------------------
-temp = stockAndSales.groupby(['CategoryID','Store']).agg({'SalesR':'sum', 'SalesQ':'sum'}).reset_index()
-temp.rename({'SalesR':'Total_CatSalesR', 'SalesQ':'Total_CatSalesQ'}, axis=1, inplace=True)
-stockAndSales = pd.merge(stockAndSales, temp, on=['CategoryID','Store'], how='left').sort_values(by=['Store', 'CategoryID'])
-del temp
-
-# -Total Sales per Store-----------------------------------
-temp = stockAndSales.groupby(['Store']).agg({'SalesR':'sum'}).reset_index()
-temp.rename({'SalesR':'Total_StoreSalesR'}, axis=1, inplace=True)
-stockAndSales = pd.merge(stockAndSales, temp, on=['Store'], how='left').sort_values(by=['Store', 'CategoryID'])
-del temp
-
-# -Aggregation---------------------------------------------
-stockAndSales['SalesAUP'] = stockAndSales.SalesR / stockAndSales.SalesQ
-stockAndSales['Total_SalesAUP'] = stockAndSales.Total_CatSalesR / stockAndSales.Total_CatSalesQ
-stockAndSales['SellOff'] = stockAndSales.SalesQ / (stockAndSales.StockQ + stockAndSales.SalesQ)
-
-stockAndSales['CatSalesRatio'] = stockAndSales.Total_CatSalesR / stockAndSales.Total_StoreSalesR # Profiles
-stockAndSales['WeekSalesRatio'] = stockAndSales.SalesR / stockAndSales.Total_CatSalesR # Profiles_Sales Ratio
-
-# -Temporary added manually--------------------------------
-stockAndSales['DesiredSellOff'] = 0.15
-stockAndSales['MLQ'] = 120
-stockAndSales['IncreaseFactor'] = 0.10
-
-# -Final Calculations--------------------------------------
-stockAndSales['s1'] = np.where(stockAndSales.SellOff > stockAndSales.DesiredSellOff, 1, np.where(stockAndSales.StockQ <= stockAndSales.MLQ, 1, 0))
-stockAndSales['s2'] = np.where(stockAndSales.s1 == 1, np.where(stockAndSales.WeekSalesRatio-stockAndSales.CatSalesRatio > 0, 1, 0), 0)
-stockAndSales['s2'] = np.where(stockAndSales.s1 == 1, np.where(stockAndSales.WeekSalesRatio-stockAndSales.CatSalesRatio > 0, 1, 0), 0)
-
-stockAndSales['ConditionCalc'] = np.where(stockAndSales.s2 == 0, stockAndSales.SalesR, 0)
-stockAndSales['ConditionMix'] = np.where(stockAndSales.s2 == 0, stockAndSales.WeekSalesRatio, 0)
-stockAndSales['SalesAdjusted'] = stockAndSales.ConditionCalc / stockAndSales.ConditionMix
-stockAndSales['SalesAdjusted'].fillna(0, inplace=True)
-
-stockAndSales['s3_Ret'] = np.where(stockAndSales.s2 == 0, 0, stockAndSales.WeekSalesRatio * stockAndSales.SalesAdjusted - stockAndSales.SalesR)
-stockAndSales['s4_Ret'] = np.where(stockAndSales.s1 == 1, np.where(stockAndSales.WeekSalesRatio - stockAndSales.CatSalesRatio < 0, stockAndSales.SalesR * stockAndSales.IncreaseFactor, 0), 0)
-stockAndSales['s4_Q'] = (stockAndSales.s3_Ret + stockAndSales.s4_Ret) / np.where(stockAndSales.SalesAUP == 0, stockAndSales.Total_SalesAUP, stockAndSales.SalesAUP)
-
-stockAndSales.head().to_csv(folderPath + '/final_df.csv', index=False)
+root.mainloop()
